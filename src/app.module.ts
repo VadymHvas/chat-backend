63c2e3cf69d07e8674e01838c6596c5140b8d0ca
novakoriss <hvassrim@gmail.com>
2024-05-05 17:20:16 +0300

    Implement registration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
 import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [ChatModule],
+  imports: [ConfigModule.forRoot(), ChatModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from "node:process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
-  await app.listen(8080);
+  await app.listen(8000);
 }
 bootstrap();
